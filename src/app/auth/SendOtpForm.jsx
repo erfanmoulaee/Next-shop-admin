81edd09a20803f907f0bfa6ecf6e61880a02c05c
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-03-17 08:54:09 +0330

    improve sendOTPform component

--- src/app/auth/SendOtpForm.jsx
+++ src/app/auth/SendOtpForm.jsx
@@ -1,13 +1,19 @@
 import TextField from "@/common/TextField";
 
-function SendOtpForm({ phoneNumber, onchange, onSubmit }) {
+function SendOtpForm({ phoneNumber, onchange, onSubmit, isLoading }) {
   return (
     <div>
       <form className="space-y-10" onSubmit={onSubmit}>
         <TextField label="شماره موبایل" name="phoneNumber" value={phoneNumber} onChange={onchange} />
-        <button type="submit" className="btn btn--primary w-full">
-          ارسال کد تائید
-        </button>
+        <div>
+          {isLoading ? (
+            <p>...loading</p>
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              ارسال کد تائید
+            </button>
+          )}
+        </div>
       </form>
     </div>
   );
--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import SendOtpForm from "./SendOtpForm";
-import http from "@/services/httpService";
 import toast from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
 import { getOtp } from "@/services/authService";
@@ -21,15 +20,17 @@ function AuthPage() {
     //call getOTP API
     try {
       const data = await mutateAsync({ phoneNumber });
+      console.log(data);
       toast.success(data.message);
     } catch (error) {
       toast.error(error?.response?.data?.message);
+      console.log(error?.response?.data?.message);
     }
   };
   return (
     <div className="flex justify-center">
       <div className="w-full sm:max-w-sm">
-        <SendOtpForm phoneNumber={phoneNumber} onchange={phoneNumberHandler} onSubmit={sendOTPHandler} />
+        <SendOtpForm phoneNumber={phoneNumber} onchange={phoneNumberHandler} onSubmit={sendOTPHandler} isLoading={isPending} />
       </div>
     </div>
   );
