cd4f73f17f6c970856a820ff962ff2887f88c416
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-04-09 23:08:19 +0330

    create access token based on refresh token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -5,6 +5,29 @@ const app = axios.create({
   withCredentials: true,
 });
 
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    console.log(err.config);
+    const orginalCofig = err.config;
+    if (err.response.status === 401 && !orginalCofig._retry) {
+      orginalCofig._retry = true;
+      try {
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/refresh-token`, { withCredentials: true });
+        if (data) return app(orginalCofig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
 const http = {
   get: app.get,
   post: app.post,
