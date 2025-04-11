154128b15cf948a3cda0dac27d94e23d7a468dac
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-04-11 17:27:31 +0330

    fetch user in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,23 @@
-export function middleware(req) {
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
   const url = req.url;
   const pathname = req.nextUrl.pathname;
 
   if (pathname.startsWith("/profile")) {
-    console.log("this is profile request");
+    let strCookies = "";
+    req.cookies.getAll().forEach((item) => {
+      strCookies += `${item?.name}=${item?.value}; `;
+    });
+    const { data } = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/profile`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Cookies: strCookies,
+      },
+    }).then((res) => res.json);
+    const { user } = data || {};
+    if (!user) return NextResponse.redirect(new URL("/auth", req.url));
   }
   if (pathname.startsWith("/admin")) {
     console.log("this is admin request");
@@ -11,5 +25,5 @@ export function middleware(req) {
 }
 
 export const config = {
-  matcher: ["/admin", "/profile"],
+  matcher: ["/admin/:path*", "/profile/:path*"],
 };
