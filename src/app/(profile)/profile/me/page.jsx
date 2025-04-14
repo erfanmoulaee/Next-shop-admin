a8058e7ab2af708a090dce199e8a11693ff53c04
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-04-14 20:58:50 +0330

    loading component

--- src/app/(profile)/profile/me/page.jsx
+++ src/app/(profile)/profile/me/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loading from "@/common/Loading";
 import TextField from "@/common/TextField";
 import { useGetUser } from "@/hooks/useAuth";
 import { includeObj } from "@/utils/objectutils";
@@ -16,7 +17,7 @@ function MePage() {
     if (user) setFormData(includeObj(user, includeKey));
   }, [user]);
 
-  if (isLoading) return <p>Loading ....</p>;
+  if (isLoading) return <Loading />;
   return (
     <div className="max-w-sm">
       <h1>اطلاعات کاربری</h1>
