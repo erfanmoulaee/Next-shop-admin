4a4f0362b3e68eb89b56d61a03c6e6387d37ee57
erfanmoulaee <erfanmoulaee1375@gmail.com>
2025-05-09 10:40:05 +0330

    post new product to backend

--- src/app/(admin)/admin/products/add/page.jsx
+++ src/app/(admin)/admin/products/add/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Select from "react-select";
 const productsFormData = [
   {
     id: 1,
@@ -48,9 +49,19 @@ const productsFormData = [
 ];
 
 import TextField from "@/common/TextField";
+import { useGetCategories } from "@/hooks/useCategories";
 import React, { useState } from "react";
+import { TagsInput } from "react-tag-input-component";
+import { useAddProduct } from "@/hooks/useProducts";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Loading from "@/common/Loading";
 
 function addProductPage() {
+  const router = useRouter();
+  const { data } = useGetCategories();
+  const { categories } = data || {};
+  const { isLoading, mutateAsync } = useAddProduct();
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -68,14 +79,39 @@ function addProductPage() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { message } = await mutateAsync({
+        ...formData,
+        tags,
+        category: selectedCategory._id,
+      });
+      router.push("/admin/products");
+      toast.success(message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
-    <div className="w-full max-w-sm">
+    <div className="w-full max-w-sm mb-10">
       <h1>اضافه کردن محصول</h1>
-      <form>
+      <form className="space-y-4" onSubmit={handleSubmit}>
         {productsFormData.map((item) => {
           return <TextField key={item.id} label={item.label} name={item.name} value={formData[item.name]} onChange={handleChange} />;
         })}
-        <button className="btn btn--primary w-full">اضافه کردن محصول</button>
+        <div>
+          <label htmlFor="tags">تگ محصولات</label>
+          <TagsInput id="tags" value={tags} onChange={setTags} name="tags" />
+        </div>
+        <div>
+          <label htmlFor="category" className="mb-2">
+            دسته بندی
+          </label>
+          <Select id="category" onChange={setSelectedCategory} options={categories} getOptionLabel={(option) => option.title} getOptionValue={(option) => option._id} />
+        </div>
+        <div>{isLoading ? <Loading /> : <button className="btn btn--primary w-full">اضافه کردن محصول</button>}</div>
       </form>
     </div>
   );
---
+++ src/hooks/useCategories.js
@@ -0,0 +1,10 @@
+import { getCategories } from "@/services/getCategories";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetCategories = () =>
+  useQuery({
+    queryKey: ["get-categories"],
+    queryFn: getCategories,
+    retry: false,
+    refetchOnWindowFocus: true,
+  });
